e26c322ff6b1662759abfbec9b4fae9c7fe47a96
daiy1-lab <daiy1@sfusd.edu>
2026-08-10 00:45:55 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GOOGLE_SHEET_WEB_APP_URL = "";
+window.GOOGLE_SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwIwzQ4M2ESlML8h7Lk6zf7opSas-Qv1YNVjbWrM2UzetzJ4aIJ8ZSreN4nqihhI64/exec";
